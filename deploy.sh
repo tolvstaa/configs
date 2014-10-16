736a1285e0e407cb8714869c78a2d20a120d4f14
Inityx <CTGBandy@gmail.com>
2014-10-15 23:41:56 -0700

    Added deploy script, renamed bashrc

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#List of compatible configs:
+#	bashrc
+#	vimrc
+#	awesome rc.lua
+
+echo "No deployment code implemented yet!"
--- readme.md
+++
@@ -1,1 +0,0 @@
-This is a repository for all application configuration files.
