7f75d2fd118452b4624ce32890ef17c6806e7e9b
tolvstaa <tolvstaa@osuosl.org>
2015-06-04 12:47:14 -0700

    removed screenshot after lock

--- specific/i3/i3lockscript.sh
+++ specific/i3/i3lockscript.sh
@@ -11,6 +11,7 @@ then
 	scrot /tmp/i3lockscrot.png
 	mogrify -spread 10 -blur 0x8 -level -5% /tmp/i3lockscrot.png
 	i3lock -i /tmp/i3lockscrot.png
+	rm -f /tmp/i3lockscrot.png
 else
 	i3lock -c 666666
 fi
