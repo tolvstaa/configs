32b3b318332a1bc5a844072898d6ff3c608b01e8
Andrew Tolvstad <CTGBandy@gmail.com>
2015-07-27 10:47:22 -0700

    Replace if/else with &&/||

--- deploy.sh
+++ deploy.sh
@@ -15,70 +15,59 @@ printf "\n###### Inityx's Configs ######\n\n"
 
 # define configurations directory
 confdir=$(printf $0 | rev | cut -d" " -f1 | cut -d"/" -f2- | rev)
-
-if [ "$confdir" != "." ]; then printf "Working with directory '${HL}${confdir}${RS}'\n"; fi
+[ "$confdir" != "." ] && printf "Working with directory '${HL}${confdir}${RS}'\n";
 
 # define index file
-if [ "$confdir" = "." ]; then indexfile=$confname
-else indexfile=${confdir}/$confname; fi
+[ "$confdir" = "." ] && indexfile=$confname || indexfile=${confdir}/$confname
 
 # validate index file
-if [ ! -e $indexfile ]; then
-	# file dne
-	printf "Index file '${ERR}${indexfile}${RS}' not found. Aborting.\n\n"
-	exit 1
-elif [ ! -r $indexfile ] || [ ! -s $indexfile ]; then
-	# file unreadable
-	printf "Index file '${ERR}${indexfile}${RS}' could not be read. Aborting.\n\n"
-	exit 1
-fi
+[ ! -e $indexfile ] && # file dne
+	( printf "Index file '${ERR}${indexfile}${RS}' not found. Aborting.\n\n"; exit 1)
+	
+( [ ! -r $indexfile ] || [ ! -s $indexfile ] ) && # file unreadable
+	( printf "Index file '${ERR}${indexfile}${RS}' could not be read. Aborting.\n\n"; exit 1)
 
 printf "Using index file '${HL}${indexfile}${RS}'\n\n"
 
 
 
 # validate index syntax
-if ! isCat $(head $indexfile -n1); then
-	printf "Incorrect syntax at head of index '${ERR}${indexfile}${RS}'. Aborting.\n\n"
-	exit 1;
-fi
+isCat $(head $indexfile -n1) ||
+	( printf "Incorrect syntax at head of index '${ERR}${indexfile}${RS}'. Aborting.\n\n"; exit 1 )
 
 # validate index contents
 cat=""
 item=""
 broken=false
 while read line; do
-	if [ "$(echo $line | awk '{print NF}')" -ne 0 ]; then
-		if isCat $line; then
+	[ "$(echo $line | awk '{print NF}')" -ne 0 ] && (
+		isCat $line && (
 			# validate category
 			cat=${line:2}
-			if [ ! -d ${confdir}/$cat ]; then
+			[ -d ${confdir}/$cat ] || (
 				printf "Configuration mismatch: "
 				printf "Directory '${ERR}${cat}${RS}' not in '${ERR}${confdir}${RS}'.\n"
 				broken=true
-			fi
-		else
+			)
+		) || (
 			# validate item
 			item=$(echo $line | awk '{print $1}')
-			if [ "$(echo $line | awk '{print NF}')" -eq 2 ]; then
-				if [ ! -r ${confdir}/${cat}/$item ]; then
+			[ "$(echo $line | awk '{print NF}')" -eq 2 ] && (
+				[ -r ${confdir}/${cat}/$item ] || (
 					printf "Configuration mismatch: "
 					printf "File '${ERR}${item}${RS}' not in '${ERR}${confdir}/${cat}${RS}'.\n"
 					broken=true
-				fi
-			else
+				)
+			) || (
 				printf "Configuration syntax error: "
 				printf "Incorrect parameters for '${ERR}${cat}/${item}${RS}'.\n"
 				broken=true
-			fi
-		fi
-	fi
+			)
+		)
+	)
 done < $indexfile
 
-if [ "$broken" = true ]; then
-	printf "\nErrors detected in config file. Aborting.\n\n"
-	exit 1
-fi
+[ "$broken" = true ] && ( printf "\nErrors detected in config file. Aborting.\n\n"; exit 1 )
 
 
 # get categories
